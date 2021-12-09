2a63d495c8c142817acc031622f1c04355e9f006
Dominic Flocco <dominicflocco@dominics-mbp.wireless.davidson.edu>
2021-12-09 17:44:29 -0500

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -64,3 +64,14 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS pitcherSeasonStats;
+DELIMITER //
+CREATE PROCEDURE pitcherSeasonStats(start_year VARCHAR(4), end_year VARCHAR(4), firstname VARCHAR(20), lastname VARCHAR(20))
+BEGIN
+	DECLARE pid INT;
+	SET pid = (SELECT player_id FROM Players WHERE (first_name = firstname AND last_name = lastname));
+	SELECT * FROM PitcherSeasonStats WHERE player_id = pid AND season >= start_year AND season <= end_year;
+END
+//
+DELIMITER ;
