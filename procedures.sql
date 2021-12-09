a12184b33ec38348fc4848b71523006871265695
Dominic Flocco <dominicflocco@dominics-mbp.wireless.davidson.edu>
2021-12-09 18:33:48 -0500

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -75,3 +75,112 @@ BEGIN
 END
 //
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS pitcherSeasonStats;
+DELIMITER //
+CREATE PROCEDURE pitcherSeasonStats(start_year VARCHAR(4), end_year VARCHAR(4), firstname VARCHAR(20), lastname VARCHAR(20))
+BEGIN
+	DECLARE pid INT;
+	SET pid = (SELECT player_id FROM Players WHERE (first_name = firstname AND last_name = lastname));
+	SELECT * FROM PitcherSeasonStats WHERE player_id = pid AND season >= start_year AND season <= end_year;
+END
+//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS batterSeasonStats;
+DELIMITER //
+CREATE PROCEDURE batterSeasonStats(start_year VARCHAR(4), end_year VARCHAR(4), firstname VARCHAR(20), lastname VARCHAR(20))
+BEGIN
+	DECLARE pid INT;
+	SET pid = (SELECT player_id FROM Players WHERE (first_name = firstname AND last_name = lastname));
+	SELECT * FROM BatterSeasonStats WHERE player_id = pid AND season >= start_year AND season <= end_year;
+END
+//
+DELIMITER ;
+
+-- Add aggregate stats 
+DROP PROCEDURE IF EXISTS batterMatchUp; 
+DELIMITER //
+CREATE PROCEDURE batterMatchUp(firstname VARCHAR(20), lastname VARCHAR(20), opponent VARCHAR(3), start_date DATE, end_date DATE, home BIT, away BIT)
+BEGIN
+	DECLARE pid INT;
+	SET pid = (SELECT player_id FROM Players WHERE (first_name = firstname AND last_name = lastname));
+	IF opponent = 'All' THEN 
+		IF home = 1 THEN
+			SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND B.team = G.home_team AND G.game_id = B.game_id;
+		ELSEIF away = 1 THEN
+			SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND B.team = G.away_team AND G.game_id = B.game_id;
+		ELSE
+			SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND (B.team = G.home_team OR B.team = G.away_team)AND G.game_id = B.game_id;
+		END IF;	
+	ELSE 
+		IF home = 1 THEN
+			SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date AND Game.away_team = opponent) AS G 
+			WHERE B.player_id = pid AND B.team = G.home_team AND G.game_id = B.game_id;
+		ELSEIF away = 1 THEN
+			SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date AND Game.home_team = opponent) AS G 
+			WHERE B.player_id = pid AND B.team = G.home_team AND G.game_id = B.game_id;
+		ELSE
+			SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND ((B.team = G.home_team AND G.away_team = opponent) OR (B.team = G.away_team AND G.home_team = opponent)) AND G.game_id = B.game_id;
+		END IF;
+	END IF;
+END;
+//
+DELIMITER ;
+-- Add aggregate
+DROP PROCEDURE IF EXISTS pitcherMatchUp; 
+DELIMITER //
+CREATE PROCEDURE pitcherMatchUp(firstname VARCHAR(20), lastname VARCHAR(20), opponent VARCHAR(3), start_date DATE, end_date DATE, home BIT, away BIT)
+BEGIN
+	DECLARE pid INT;
+	SET pid = (SELECT player_id FROM Players WHERE (first_name = firstname AND last_name = lastname));
+	IF opponent = 'All' THEN 
+		IF home = 1 THEN
+			SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND B.team = G.home_team AND G.game_id = B.game_id;
+		ELSEIF away = 1 THEN
+			SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND B.team = G.away_team AND G.game_id = B.game_id;
+		ELSE
+			SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND (B.team = G.home_team OR B.team = G.away_team)AND G.game_id = B.game_id;
+		END IF;	
+	ELSE 
+		IF home = 1 THEN
+			SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date AND Game.away_team = opponent) AS G 
+			WHERE B.player_id = pid AND B.team = G.home_team AND G.game_id = B.game_id;
+		ELSEIF away = 1 THEN
+			SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date AND Game.home_team = opponent) AS G 
+			WHERE B.player_id = pid AND B.team = G.home_team AND G.game_id = B.game_id;
+		ELSE
+			SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G 
+			WHERE B.player_id = pid AND ((B.team = G.home_team AND G.away_team = opponent) OR (B.team = G.away_team AND G.home_team = opponent)) AND G.game_id = B.game_id;
+		END IF;
+	END IF;
+END;
+//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS batterLeaderboard;
+DELIMITER //
+CREATE PROCEDURE batterLeaderboard(start_date DATE, end_date DATE)
+BEGIN
+	SELECT * FROM BatterPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G, Players AS P 
+	WHERE B.game_id = G.game_id AND P.player_id = B.player_id;
+END
+//
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS pitcherLeaderboard;
+DELIMITER //
+CREATE PROCEDURE pitcherLeaderboard(start_date DATE, end_date DATE)
+BEGIN
+	SELECT * FROM PitcherPlaysIn AS B, (SELECT * FROM Game WHERE Game.date_game >= start_date AND Game.date_game <= end_date) AS G, Players AS P 
+	WHERE B.game_id = G.game_id AND P.player_id = B.player_id;
+END
+//
+DELIMITER ;
